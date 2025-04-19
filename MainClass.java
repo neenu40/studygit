51ec59a4e8cd14af411c5d03c2db4f0e63050d9a
neenu40 <neenuedwin@gmail.com>
2025-04-19 06:16:03 -0400

    added main class jvav in john branch

---
+++ MainClass.java
@@ -0,0 +1,14 @@
+package com.test.classes;
+
+public class MainClass {
+	public static void main(String[] args) {
+        Person person = new Person("John Smith", 25, "johnsmith@example.com");
+        person.displayDetails();
+        
+        Employee employee = new Employee("John Doe", 25, "johndoe@example.com", 12345);
+        employee.displayEmployeeDetails();
+        
+        employee.isEmployeeEligibleForRetirement(78);
+system.out.println("hello from neenu");
+    }
+}
